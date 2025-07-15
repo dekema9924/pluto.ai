6cc0a645d1f0ec7f517131a6d198f4bd33966174
dekema9924 <danekema9924@gmail.com>
2025-07-14 21:52:03 -0700

    hero section

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,26 @@
+
+
+const Header = () => {
+    return (
+        <>
+            <header className="flex items-center justify-between h-44 ">
+                {/* //logo */}
+                <div className="flex items-center ml-10">
+                    <div>
+                        <span className=" w-4 h-4 bg-[wheat] rounded-full block"></span>
+                        <span className=" w-4 h-4 mx-6 bg-[wheat] rounded-full block"></span>
+                        <span className=" w-4 h-4 bg-[wheat] rounded-full block"></span>
+                    </div>
+                    <h1 className="text-[2em] md:text-[3em] font-semibold text-[wheat]">Pluto.ai</h1>
+                </div>
+
+                <button className="border-2 cursor-pointer mr-10 w-42 h-12 rounded-md bg-[wheat] text-black font-bold">Get Started</button>
+
+
+            </header>
+        </>
+    )
+}
+
+
+export default Header
