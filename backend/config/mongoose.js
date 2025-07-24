808aa4c33f60209e5beaf0317b15be268cc5d3fe
dekema9924 <danekema9924@gmail.com>
2025-07-24 10:17:36 -0700

    mongoose url err

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const MongoURl = process.env.NODE_ENV == 'production' ? process.env.PROD_MONGOOSE_URL : process.env.MONGOOSE_URL
 
 try {
-    mongoose.connect(`${MongoURl}/plutoai`).then(() => {
+    mongoose.connect(MongoURl).then(() => {
         console.log('Mongoose connected to plutoai database');
 
     })
