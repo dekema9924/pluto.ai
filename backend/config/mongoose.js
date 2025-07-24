15e451e5f6a7828eedb54ce1959ee68a7a036799
dekema9924 <danekema9924@gmail.com>
2025-07-23 21:21:57 -0700

    update env

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -2,13 +2,15 @@
 
 const mongoose = require('mongoose');
 
-const MongoURl = process.env.NODE_ENV == 'production' ? PROD_MONGOOSE_URL : process.env.MONGOOSE_URL
+const MongoURl = process.env.NODE_ENV == 'production' ? process.env.PROD_MONGOOSE_URL : process.env.MONGOOSE_URL
 console.log(MongoURl)
 
 try {
     mongoose.connect(`${MongoURl}/plutoai`).then(() => {
-        console.log('mongoose connected')
+        console.log('Mongoose connected to plutoai database');
+
     })
 } catch (error) {
-    console.log(error)
+    console.error('Mongoose connection error:', error);
+
 }
